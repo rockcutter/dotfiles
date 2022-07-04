5819b28c9dd873aa94ce30d995096e702b434182
rockcutter <rockcutter414@gmail.com>
2022-07-04 23:55:41 +0900

    [change] Plugを自動でインストールしないように変更

--- setup.bat
+++ setup.bat
@@ -2,9 +2,6 @@
 mkdir $HOME\Documents\PowerShell\
 cp powershell\Microsoft.PowerShell_profile.ps1 $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 
-iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
-
 #neovim
 mkdir $HOME\AppData\Local\nvim
 cp nvim\init.vim $HOME\AppData\Local\nvim\init.vim
