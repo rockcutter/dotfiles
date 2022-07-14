70d2914958fd593f690eb39bef4810319823cb24
rockcutter <rockcutter414@gmail.com>
2022-07-14 17:30:25 +0900

    [change] setup.batをsetup.ps1に名称変更

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,7 @@
-#powershell
+# powershell
 mkdir $HOME\Documents\PowerShell\
 cp powershell\Microsoft.PowerShell_profile.ps1 $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 
-#neovim
+# neovim
 mkdir $HOME\AppData\Local\nvim
 cp nvim\init.vim $HOME\AppData\Local\nvim\init.vim
