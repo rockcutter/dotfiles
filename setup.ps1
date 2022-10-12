4d83cee1d97eb6bcd71ecff953a689e9a3a49de1
rockcutter <rockcutter414@gmail.com>
2022-10-12 18:50:52 +0900

    [fix] cpをCopy-Itemに変更

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,7 @@
 # powershell
 mkdir $HOME\Documents\PowerShell\
-cp powershell\Microsoft.PowerShell_profile.ps1 $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+Copy-Item powershell\Microsoft.PowerShell_profile.ps1 $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 
 # neovim
 mkdir $HOME\AppData\Local\nvim
-cp nvim\init.vim $HOME\AppData\Local\nvim\init.vim
+Copy-Item nvim\init.vim $HOME\AppData\Local\nvim\init.vim
