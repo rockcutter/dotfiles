231829adc212d5f8765045abcb77ca0ccdd5a5bf
rockcutter <rockcutter414@gmail.com>
2022-10-12 19:08:48 +0900

    [add] Powershell7でないPowershell用にもprofileを追加

---
+++ powershell/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+
+Set-Alias ll ls
+Set-Alias  touch New-Item
+Set-Alias grep findstr
--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,11 @@
 # powershell
 mkdir $HOME\Documents\PowerShell\
-Copy-Item powershell\Microsoft.PowerShell_profile.ps1 $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+Copy-Item $PSScriptRoot\powershell\powershell7\Microsoft.PowerShell_profile.ps1 $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+
+# powershell(old)
+mkdir $HOME\Documents\WindowsPowerShell\
+Copy-Item $PSScriptRoot\powershell\PowerShell\Microsoft.PowerShell_profile.ps1 $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 
 # neovim
 mkdir $HOME\AppData\Local\nvim
-Copy-Item nvim\init.vim $HOME\AppData\Local\nvim\init.vim
+Copy-Item $PSScriptRoot\nvim\init.vim $HOME\AppData\Local\nvim\init.vim
