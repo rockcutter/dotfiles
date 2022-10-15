1ce05726b913e1f899e61a181af21838de57d92f
rockcutter <rockcutter414@gmail.com>
2022-10-15 14:13:51 +0900

    [clean] リファクタリングを実施

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -33,9 +33,9 @@ function prompt {
 	$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
 	$principal = [Security.Principal.WindowsPrincipal] $identity
 	$adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+
 	#definition
 	$my_color_1 = "DarkRed"
-
 
 	$shell_wallpaper_color = "Black"
 	$shell_background_color = $my_color_1
@@ -54,28 +54,40 @@ function prompt {
 
 	
 	Write-Host("┌─") -NoNewline -ForegroundColor Green
-	#shell
+
+	#
+	# write shell ----------------------------------------------------------------------------------
+	#
+	
 	Write-Host("`u{e0b6}") -ForegroundColor $shell_background_color -NoNewline
 	Write-Host ("`u{fab2} pwsh") -nonewline -ForegroundColor $shell_foreground_color -BackgroundColor $shell_background_color
-	#Write-Host("") -NoNewline -ForegroundColor $shell_background_color -BackgroundColor $user_background_color
-	#shell-user separator
 	Write-Host("") -NoNewline -ForegroundColor $shell_background_color -BackgroundColor $shell_wallpaper_color
-	Write-Host("") -NoNewline -ForegroundColor $shell_wallpaper_color -BackgroundColor $user_background_color
-	#user	
-	$username = if($principal.IsInRole($adminRole)){
-		"`u{f084} admin"
-	}elseif($myprofile_optional_username){
-		"`u{f2be} $myprofile_optional_username"
-	}elseif($myprofile_hide_username) {
-		"`u{f2be} <hidden>"	
-	}else{
-		"`u{f2be} $env:USERNAME"
+	
+	#
+	# shell end ----------------------------------------------------------------------------------
+	#
 
+	#
+	# Write user	-------------------------------------------------------------------------------------
+	# 
+	
+	$username = if($principal.IsInRole($adminRole)){
+		" admin"
+	}elseif($myprofile_optional_username){
+		" $myprofile_optional_username"
+	}elseif($myprofile_hide_username) {
+		" <hidden>"	
+	}else{
+		" $env:USERNAME"
 	}
-	#Write-Host("`u{f2be} ") -NoNewline -BackgroundColor $user_background_color -ForegroundColor $user_foreground_color
-	Write-Host($username + "`u{e79b} " + $env:COMPUTERNAME + " ") -NoNewline -BackgroundColor $user_background_color -ForegroundColor $user_foreground_color
-	#user-path separator
+
+	Write-Host("") -NoNewline -ForegroundColor $shell_wallpaper_color -BackgroundColor $user_background_color
+	Write-Host($username + " " + $env:COMPUTERNAME + " ") -NoNewline -BackgroundColor $user_background_color -ForegroundColor $user_foreground_color
 	Write-Host("") -NoNewline -ForegroundColor $user_background_color -BackgroundColor $shell_wallpaper_color
+
+	#
+	# Write user end -------------------------------------------------------------------------------------
+	# 
 
 	#
 	# Write git branch -------------------------------------------------------------------------
@@ -92,39 +104,58 @@ function prompt {
 	# git branch end -------------------------------------------------------------------------
 	#
 
-	#path
+	#
+	# Write path -------------------------------------------------------------------------
+	#
+	
+	$path_string = "$($executionContext.SessionState.Path.CurrentLocation)".Replace("$env:HOMEDRIVE$env:HOMEPATH\source\repos", "`u{f121} ").Replace("$env:HOMEDRIVE$env:HOMEPATH", "`u{f015} ").Replace("\", "`u{e0bd} ").Replace(":", "")
+	
 	Write-Host("") -NoNewline -ForegroundColor $shell_wallpaper_color -BackgroundColor $path_background_color
-	Write-Host(" `u{f115} `u{f63d} ") -NoNewline -BackgroundColor $path_background_color -ForegroundColor $path_foreground_color
-	Write-Host("$($executionContext.SessionState.Path.CurrentLocation)".Replace("$env:HOMEDRIVE$env:HOMEPATH\source\repos", "`u{f121} ").Replace("$env:HOMEDRIVE$env:HOMEPATH", "`u{f015} ").Replace("\", "`u{e0bd} ").Replace(":", "")) -nonewline -ForegroundColor $path_foreground_color -BackgroundColor $path_background_color
-	#Write-Host("`u{e0b4}") -ForegroundColor $path_background_color #これは丸の右半分
-	Write-Host("") -nonewline -ForegroundColor $path_background_color #これは▶
-	#Write-Host("`u{e0c4}") -ForegroundColor $path_background_color #これはげじげじ
+	Write-Host("   ") -NoNewline -BackgroundColor $path_background_color -ForegroundColor $path_foreground_color
+	Write-Host($path_string) -nonewline -ForegroundColor $path_foreground_color -BackgroundColor $path_background_color
+	Write-Host("") -nonewline -ForegroundColor $path_background_color 
+	
+	#
+	# Write path end -------------------------------------------------------------------------
+	#
 	
 	Write-Host("".PadLeft(5)) -NoNewline
-	#return
+
+	#
+	# check return val -----------------------------------------------------------------------
+	#
+	
 	$last_return_color = ""
 	$last_return_value = ""
 	if($last_return){
 		$last_return_color = $return_code_success_color
-		$last_return_value = "`u{fb0c} `u{f62b} "
+		$last_return_value = "﬌  "
 	}else{
 		$last_return_color = $return_code_failed_color
-		$last_return_value = "`u{fb0c} `u{f467} "
+		$last_return_value = "﬌  "
 	}
 	
-	Write-Host("`u{e0b3}") -NoNewline -ForegroundColor $last_return_color
-	Write-Host("`u{e0b2}") -NoNewline -ForegroundColor $last_return_color
+	Write-Host("") -NoNewline -ForegroundColor $last_return_color
 	Write-Host($last_return_value) -NoNewline -ForegroundColor $return_foreground_color -BackgroundColor $last_return_color
+	Write-Host("") -NoNewline -ForegroundColor $shell_wallpaper_color -BackgroundColor $last_return_color
 
-	#return-time separator
-	Write-Host("`u{e0b2}") -NoNewline -ForegroundColor $shell_wallpaper_color -BackgroundColor $last_return_color
+	#
+	# check return val end -----------------------------------------------------------------------
+	#
+	
+	#
+	# write time -----------------------------------------------------------------------
+	#
 
-	#time
-	Write-Host("`u{e0b2}") -NoNewline -ForegroundColor $time_background_color
-	Write-Host("`u{f64f} ") -NoNewline -ForegroundColor $time_foreground_color -BackgroundColor $time_background_color
+	Write-Host("") -NoNewline -ForegroundColor $time_background_color
+	Write-Host(" ") -NoNewline -ForegroundColor $time_foreground_color -BackgroundColor $time_background_color
 	Write-Host((Get-Date -Format "hh:mm:ss")) -NoNewline -ForegroundColor $time_foreground_color -BackgroundColor $time_background_color
-	Write-Host("`u{e0b0}`u{e0b1}") -NoNewline -ForegroundColor $time_background_color
+	Write-Host("") -NoNewline -ForegroundColor $time_background_color
 
+	#
+	# write time end-----------------------------------------------------------------------
+	#
+	
 	#prompt
 	Write-Host 
 	$prompt_color = "Gray"
