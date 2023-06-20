f2e2ffe53d4785e63244ffb0388807ac1cbb55d2
rockcutter <rockcutter414@gmail.com>
2023-06-21 01:16:53 +0900

    [add] 実行するかユーザに問う関数を追加

--- setup.ps1
+++ setup.ps1
@@ -1,11 +1,33 @@
 # powershell
-mkdir $HOME\Documents\PowerShell\
-Copy-Item $PSScriptRoot\powershell\powershell7\Microsoft.PowerShell_profile.ps1 $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 
+Function CheckExecute(){
+	while($True){
+		$input = Read-Host "実行しますか?(y/n)"
+		if($input -eq "y"){
+			return $True
+		} 
+		if($input -eq "n"){
+			return $False
+		}
+	}
+}
+
+$execute = CheckExecute
+if($execute){
+	mkdir $HOME\Documents\PowerShell\
+	Copy-Item $PSScriptRoot\powershell\powershell7\Microsoft.PowerShell_profile.ps1 $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+}
+
+$execute = CheckExecute
 # powershell(old)
-mkdir $HOME\Documents\WindowsPowerShell\
-Copy-Item $PSScriptRoot\powershell\PowerShell\Microsoft.PowerShell_profile.ps1 $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+if($execute){
+	mkdir $HOME\Documents\WindowsPowerShell\
+	Copy-Item $PSScriptRoot\powershell\PowerShell\Microsoft.PowerShell_profile.ps1 $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+}
 
+$execute = CheckExecute
 # neovim
-mkdir $HOME\AppData\Local\nvim
-Copy-Item $PSScriptRoot\nvim\init.vim $HOME\AppData\Local\nvim\init.vim
+if($execute){
+	mkdir $HOME\AppData\Local\nvim
+	Copy-Item $PSScriptRoot\nvim\init.vim $HOME\AppData\Local\nvim\init.vim
+}
