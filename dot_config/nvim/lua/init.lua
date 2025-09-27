2e9fa24d80d5f26efda9e87d4bbf80024be63704
rockcutter <rockcutter414@gmail.com>
2025-09-27 18:17:02 +0900

    Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/init.lua Add .config/nvim/lua/keymap.lua Add .config/nvim/lua/plugin.lua Add .config/nvim/lua/prep_lazy.lua Add .config/nvim/lua/prep_noice.lua Add .config/nvim/lua/prep_telescope.lua Add .config/nvim/lua/random_colorscheme.lua Add .config/nvim/lua/vscode.lua

---
+++ dot_config/nvim/lua/init.lua
@@ -0,0 +1,58 @@
+require("~/.config/nvim/prep_lazy")
+
+require("~/.config/nvim/plugin")
+
+-- airline
+vim.g["airline#extensions#tabline#enabled"] = 1
+
+-- choose random theme
+require("~/.config/nvim/random_colorscheme")
+
+-- etc setting
+vim.cmd("set number")
+
+-- tab -> 4 space
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+require("~/.config/nvim/keymap")
+
+-- telescope
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = {
+			-- 検索から除外するものを指定
+			"^.git/",
+			"^.cache/",
+			"^.zsh_sessions/",
+			"^Library/",
+			"Parallels",
+			"^Movies",
+			"^Music",
+			"Dropbox/",
+			".DS_Store",
+		},
+		vimgrep_arguments = {
+			-- ripggrepコマンドのオプション
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			"-uu",
+		},
+	},
+	extensions = {
+		-- ソート性能を大幅に向上させるfzfを使う
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case",
+		},
+	},
+})
+require("telescope").load_extension("fzf")
---
+++ dot_config/nvim/lua/plugin.lua
@@ -0,0 +1,87 @@
+require("lazy").setup({
+	{
+		"vim-airline/vim-airline",
+		airline_powerline_fonts = 1,
+	},
+	{
+		"prabirshrestha/vim-lsp",
+	},
+	{
+		"airblade/vim-gitgutter",
+	},
+	{
+		"mattn/vim-lsp-settings",
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+-- 	{
+-- 		"folke/noice.nvim",
+-- 		event = "VeryLazy",
+-- 		config = function()
+-- 			require("prep_noice")
+-- 		end,
+-- 		opts = {
+-- 			-- add any options here
+-- 		},
+-- 		dependencies = {
+-- 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+-- 			"MunifTanjim/nui.nvim",
+-- 			-- OPTIONAL:
+-- 			--   `nvim-notify` is only needed, if you want to use the notification view.
+-- 			--   If not available, we use `mini` as the fallback
+-- 			"rcarriga/nvim-notify",
+-- 		},
+-- 	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		filesystem = {
+			hijack_netrw_behavior = "open_current",
+			filtered_items = {
+				visible = false,
+				hide_dotfiles = false,
+				hide_gitignored = true,
+			},
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- Optional image support for file preview: See `# Preview Mode` for more information.
+			-- {"3rd/image.nvim", opts = {}},
+			-- OR use snacks.nvim's image module:
+			-- "folke/snacks.nvim",
+		},
+		lazy = false, -- neo-tree will lazily load itself
+		---@module "neo-tree"
+		---@type neotree.Config?
+		opts = {
+			-- add options here
+		},
+	},
+	{
+		"voldikss/vim-floaterm",
+	},
+	{
+		"tpope/vim-surround",
+	},
+
+	-- theme
+	{
+		"ajmwagar/vim-deus",
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+	},
+	{
+		"morhetz/gruvbox",
+	},
+})
