034725a5388e1ec4bd9d0f00f1085944afe6497b
rockcutter <rockcutter414@gmail.com>
2025-11-19 11:41:56 +0900

    Update .config/nvim/init.lua Update .zshrc

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -21,9 +21,19 @@ vim.api.nvim_create_autocmd("VimResized", {
   end,
 })
 
+-- 外部でファイルが変更されたら自動的にリロード
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  callback = function()
+    if vim.fn.mode() ~= "c" then
+      vim.cmd("checktime")
+    end
+  end,
+})
 
 vim.opt.mouse = ""
 vim.opt.clipboard = "unnamedplus"
+vim.opt.autoread = true
 
 vim.opt.splitright = true
 
