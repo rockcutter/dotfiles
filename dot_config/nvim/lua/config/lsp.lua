52381ef3000dcbc48fcb4604a7fcb1f9c5af16d4
rockcutter <rockcutter414@gmail.com>
2026-01-19 20:36:30 +0900

    Update .config/nvim/lua/config/lsp.lua

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -2,147 +2,147 @@
 vim.opt.completeopt = { "noselect", "menuone", "popup" }
 
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
-  float = {
-    border = "rounded",
-    source = "always",
-  },
+	virtual_lines = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+	float = {
+		border = "rounded",
+		source = "always",
+	},
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("my.lsp", {}),
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    local opts = { buffer = args.buf }
+	group = vim.api.nvim_create_augroup("my.lsp", {}),
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		local opts = { buffer = args.buf }
 
-    -- Setup signature help
-    require("lsp_signature").on_attach({
-      bind = true,
-      handler_opts = {
-        border = "rounded",
-      },
-      hint_enable = false,
-      floating_window = true,
-      toggle_key = "<C-k>",
-    }, args.buf)
+		-- Setup signature help
+		require("lsp_signature").on_attach({
+			bind = true,
+			handler_opts = {
+				border = "rounded",
+			},
+			hint_enable = false,
+			floating_window = true,
+			toggle_key = "<C-k>",
+		}, args.buf)
 
-    if client:supports_method("textDocument/declaration") then
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    end
+		if client:supports_method("textDocument/declaration") then
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		end
 
-    vim.keymap.set("n", "gd", function()
-      require("telescope.builtin").lsp_definitions()
-    end, opts)
-    vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, opts)
-    -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
-    -- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+		vim.keymap.set("n", "gd", function()
+			require("telescope.builtin").lsp_definitions()
+		end, opts)
+		vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, opts)
+		-- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
+		-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
 
-    -- Document highlight on cursor hold
-    if client:supports_method("textDocument/documentHighlight") then
-      vim.api.nvim_create_autocmd("CursorHold", {
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.document_highlight()
-        end,
-      })
+		-- Document highlight on cursor hold
+		if client:supports_method("textDocument/documentHighlight") then
+			vim.api.nvim_create_autocmd("CursorHold", {
+				buffer = args.buf,
+				callback = function()
+					vim.lsp.buf.document_highlight()
+				end,
+			})
 
-      vim.api.nvim_create_autocmd("CursorMoved", {
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.clear_references()
-        end,
-      })
-    end
+			vim.api.nvim_create_autocmd("CursorMoved", {
+				buffer = args.buf,
+				callback = function()
+					vim.lsp.buf.clear_references()
+				end,
+			})
+		end
 
-    if client:supports_method("textDocument/implementation") then
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    end
+		if client:supports_method("textDocument/implementation") then
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		end
 
-    if client:supports_method("textDocument/completion") then
-      local chars = {}
-      for i = 32, 126 do
-        table.insert(chars, string.char(i))
-      end
-      client.server_capabilities.completionProvider.triggerCharacters = chars
-      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-    end
+		if client:supports_method("textDocument/completion") then
+			local chars = {}
+			for i = 32, 126 do
+				table.insert(chars, string.char(i))
+			end
+			client.server_capabilities.completionProvider.triggerCharacters = chars
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
 
-    -- Auto-format ("lint") on save.
-    if
-        not client:supports_method("textDocument/willSaveWaitUntil")
-        and client:supports_method("textDocument/formatting")
-    then
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
-        buffer = args.buf,
-        callback = function()
-          -- Goファイルはvim-goのgoimportsに任せる
-          if vim.bo[args.buf].filetype == "go" then
-            return
-          end
-          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-        end,
-      })
-    end
-  end,
+		-- Auto-format ("lint") on save.
+		if
+			not client:supports_method("textDocument/willSaveWaitUntil")
+			and client:supports_method("textDocument/formatting")
+		then
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
+				buffer = args.buf,
+				callback = function()
+					-- Goファイルはvim-goのgoimportsに任せる
+					if vim.bo[args.buf].filetype == "go" then
+						return
+					end
+					vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+				end,
+			})
+		end
+	end,
 })
 
 vim.lsp.config["lua_ls"] = {
-  cmd = { "lua-language-server" },
-  filetypes = { "lua" },
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = { vim.env.VIMRUNTIME },
-      },
-    },
-  },
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = { vim.env.VIMRUNTIME },
+			},
+		},
+	},
 }
 
 vim.lsp.config["gopls"] = {
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod" },
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-        shadow = true,
-      },
-      staticcheck = true,
-      gofumpt = true,
-    },
-  },
+	cmd = { "gopls" },
+	filetypes = { "go", "gomod" },
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+				shadow = true,
+			},
+			staticcheck = true,
+			gofumpt = true,
+		},
+	},
 }
 
 vim.lsp.config["graphql"] = {
-  cmd = { "graphql-lsp", "server", "-m", "stream" },
-  filetypes = { "graphql" },
+	cmd = { "graphql-lsp", "server", "-m", "stream" },
+	filetypes = { "graphql" },
 }
 
 vim.lsp.config["ts_ls"] = {
-  cmd = { "typescript-language-server", "--stdio" },
-  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-  root_markers = { "package.json", "tsconfig.json", "jsconfig.json" },
+	cmd = { "typescript-language-server", "--stdio" },
+	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+	root_markers = { "package.json", "tsconfig.json", "jsconfig.json" },
 }
 
 vim.lsp.config["clangd"] = {
-  cmd = { "clangd", "--background-index" },
-  filetypes = { "c", "cpp", "h", "hpp" },
+	cmd = { "clangd", "--background-index" },
+	filetypes = { "c", "cpp", "h", "hpp" },
 }
 
 vim.lsp.enable({
-  "lua_ls",
-  "gopls",
-  "graphql",
-  "ts_ls",
-  "clangd",
+	"lua_ls",
+	"gopls",
+	"graphql",
+	"ts_ls",
+	"clangd",
 })
