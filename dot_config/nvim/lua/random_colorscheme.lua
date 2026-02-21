a2f55dabb48d5a0d86a9cafb02c0e0c508f038c8
rockcutter <rockcutter414@gmail.com>
2026-02-21 21:18:16 +0900

    Update .config/nvim/lua/random_colorscheme.lua

--- dot_config/nvim/lua/random_colorscheme.lua
+++ dot_config/nvim/lua/random_colorscheme.lua
@@ -1,18 +1,18 @@
 function choose_random_theme(theme_list)
-  math.randomseed(os.time())
-  return theme_list[math.random(#theme_list)]
+	math.randomseed(os.time())
+	return theme_list[math.random(#theme_list)]
 end
 
 choosed_theme = choose_random_theme({
-  "tokyonight-moon",
-  "tokyonight-storm",
-  "tokyonight-night",
-  --'deus',
-  "gruvbox",
-  "ayu",
-  "ayu-dark",
-  "ayu-mirage",
-  "everforest",
+	--- "tokyonight-moon",
+	"tokyonight-storm",
+	"tokyonight-night",
+	--'deus',
+	"gruvbox",
+	"ayu",
+	"ayu-dark",
+	"ayu-mirage",
+	"everforest",
 })
 
 vim.cmd("colorscheme " .. choosed_theme)
