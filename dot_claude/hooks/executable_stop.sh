f3a31ab01d710965cd07104ae663d9719ca09db3
rockcutter <rockcutter414@gmail.com>
2026-03-21 12:00:45 +0900

    Update .claude/hooks/stop.sh Update .zshrc

--- dot_claude/hooks/executable_stop.sh
+++ dot_claude/hooks/executable_stop.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 tmux select-window -t "$TMUX_PANE"
-osascript -e 'display notification "処理が完了しました" with title "Claude Code" sound name "Glass"'
+if [[ "$(uname)" == "Darwin" ]]; then
+  osascript -e 'display notification "Claude Codeが入力を待っています" with title "Claude Code" sound name "Glass"'
+else
+  notify-send "Claude Code" "Claude Codeが入力を待っています"
+fi
 tmux display-popup -w 60% -h 20% -E "figlet -c 'claude is calling'; sleep 0.5"
