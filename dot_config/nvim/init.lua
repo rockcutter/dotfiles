6eb679964f9492b0128dabf789c06abe97da78cf
rockcutter <rockcutter414@gmail.com>
2026-04-23 00:32:44 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -13,15 +13,16 @@ vim.opt.wrap = true
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
 vim.opt.softtabstop = -1 -- tabstop に従わせる
-vim.opt.shiftwidth = 0 -- tabstop に従わせる
+vim.opt.shiftwidth = 0   -- tabstop に従わせる
 
 vim.o.laststatus = 3
 vim.opt.list = true
 vim.opt.listchars = {
-	space = "·",
-	trail = "␣",
-	tab = "  ",
+  space = "·",
+  trail = "␣",
+  tab = "  ",
 }
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,t:ver25"
 
 -- airline
 vim.g["airline#extensions#tabline#enabled"] = 1
@@ -40,8 +41,8 @@ require("lsp.autocmd")
 require("random_colorscheme")
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "<filetype>" },
-	callback = function()
-		vim.treesitter.start()
-	end,
+  pattern = { "<filetype>" },
+  callback = function()
+    vim.treesitter.start()
+  end,
 })
